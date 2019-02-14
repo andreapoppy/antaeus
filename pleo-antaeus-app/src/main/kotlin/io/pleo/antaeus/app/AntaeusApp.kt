d4eb57899171f7a26a8a073aa17bc76c2e04da05
andreapoppy <andrea.del.popolo@hotmail.it>
2019-02-14 17:21:12 +0100

    here we go, first lines of code written in Kotlin
    Added so-called coroutines library to have the BillingService running in its own thread

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -57,6 +57,7 @@ fun main() {
 
     // This is _your_ billing service to be included where you see fit
     val billingService = BillingService(paymentProvider = paymentProvider)
+    billingService.run()
 
     // Create REST web service
     AntaeusRest(
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,17 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import kotlinx.coroutines.*
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-   // TODO - Add code e.g. here
+   fun run() {
+       GlobalScope.launch {
+          while(true) {
+             delay(1000L)
+             println("BillingService is running in the background")
+          }
+       }
+   }
 }
