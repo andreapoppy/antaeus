d9de83385ed051c2f545ff4d97bdb172e6aaa581
andreapoppy <andrea.del.popolo@hotmail.it>
2019-02-15 12:08:27 +0100

    Extend BillingService to cover best case scenario where payment provider succeeds

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,8 +1,12 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.contracts.IDateTimeProvider
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.InvoiceStatus
 import kotlinx.coroutines.*
 import java.time.Duration
 
@@ -10,11 +14,24 @@ class BillingService(
         private val dal: AntaeusDal,
         private val dateTimeProvider: IDateTimeProvider,
         private val paymentProvider: PaymentProvider) {
-   fun run() {
-       GlobalScope.launch {
+
+    private var job : Job? = null
+
+    fun run() {
+       job = GlobalScope.launch {
            mainRoutine()
        }
    }
+
+    private suspend fun restart() {
+        if (job != null) {
+            job?.cancel()
+            job?.join()
+        }
+        job = GlobalScope.launch {
+            mainRoutine()
+        }
+    }
 
     private suspend fun mainRoutine() {
         while (true) {
@@ -23,7 +40,36 @@ class BillingService(
                 goToSleep(intervalInMs)
             }
 
-            // TODO: pick up from here
+            val pendingInvoices = dal.fetchInvoicesByStatus(InvoiceStatus.PENDING)
+            for (invoice in pendingInvoices) {
+                var isSuccess = false
+                try {
+                    isSuccess = paymentProvider.charge(invoice)
+                }
+                catch(e : CustomerNotFoundException) {
+                    // TODO: log and continue
+                    isSuccess = false
+                }
+                catch(e : CurrencyMismatchException) {
+                    // TODO: figure out if we can fix the currency
+                    isSuccess = false
+                }
+                catch(e : NetworkException) {
+                    // Retry later
+                    goToSleep(60 * 1000L)
+                    restart()
+                }
+
+                if (isSuccess) {
+                    dal.updateInvoiceStatus(invoice.id, InvoiceStatus.PAID)
+                }
+                else {
+                    // TODO: log
+                }
+            }
+
+            // Will check in 5 minutes if new invoices show up
+            goToSleep(5 * 60 * 1000L)
         }
     }
 
