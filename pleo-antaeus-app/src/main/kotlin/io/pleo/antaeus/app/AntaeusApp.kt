95889c5dbd0af82fe9b05dff17b760bf8e8578dc
andreapoppy <andrea.del.popolo@hotmail.it>
2019-02-15 11:08:52 +0100

    Added dependency to IDateTimeProvider in BillingService

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -8,6 +8,7 @@
 package io.pleo.antaeus.app
 
 import getPaymentProvider
+import io.pleo.antaeus.core.helpers.DateTimeProvider
 import io.pleo.antaeus.core.services.BillingService
 import io.pleo.antaeus.core.services.CustomerService
 import io.pleo.antaeus.core.services.InvoiceService
@@ -55,8 +56,16 @@ fun main() {
     val invoiceService = InvoiceService(dal = dal)
     val customerService = CustomerService(dal = dal)
 
-    // This is _your_ billing service to be included where you see fit
-    val billingService = BillingService(paymentProvider = paymentProvider)
+    // Create cross-cutting-concerns
+    val dateTimeProvider = DateTimeProvider()
+
+    // This is _my_ billing service
+    val billingService = BillingService(
+            dal = dal,
+            dateTimeProvider = dateTimeProvider,
+            paymentProvider = paymentProvider
+    )
+
     billingService.run()
 
     // Create REST web service
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,17 +1,36 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.contracts.IDateTimeProvider
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.data.AntaeusDal
 import kotlinx.coroutines.*
+import java.time.Duration
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
-) {
+        private val dal: AntaeusDal,
+        private val dateTimeProvider: IDateTimeProvider,
+        private val paymentProvider: PaymentProvider) {
    fun run() {
        GlobalScope.launch {
-          while(true) {
-             delay(1000L)
-             println("BillingService is running in the background")
-          }
+           mainRoutine()
        }
    }
+
+    private suspend fun mainRoutine() {
+        while (true) {
+
+            if (!dateTimeProvider.isFirstOfTheMonth()) {
+                val nextFirstOfTheMonth = dateTimeProvider.nextFirstOfTheMonth()
+                val intervalInMs = Duration.between(nextFirstOfTheMonth, dateTimeProvider.now()).toMillis()
+                goToSleep(intervalInMs)
+            }
+
+            // TODO: pick up from here
+        }
+    }
+
+    private suspend fun goToSleep(timeInMs : Long) {
+        println("BillingService is going to sleep in the background")
+        delay(timeInMs)
+    }
 }
